1e807bc56a10942b40aeb19c796ca2abec58d80b
Vio U <viotemp1@icloud.com>
2021-04-02 16:04:48 +0300

    my

--- trt_pose/coco.py
+++ trt_pose/coco.py
@@ -231,6 +231,7 @@ class CocoDataset(torch.utils.data.Dataset):
             self.parts = cache['parts']
             self.filenames = cache['filenames']
             self.samples = cache['samples']
+            rint('Loading from cache file done!')
             return
             
         with open(annotations_file, 'r') as f:
