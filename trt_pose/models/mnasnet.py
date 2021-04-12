ea7c072627ee6ee7bab29eb4bc91642bcfa44ed7
Vio U <viotemp1@icloud.com>
2021-04-12 09:53:15 +0300

    my

--- trt_pose/models/mnasnet.py
+++ trt_pose/models/mnasnet.py
@@ -51,21 +51,21 @@ def mnasnet1_3_baseline(cmap_channels, paf_channels, upsample_channels=256, pret
 
 
 
-def mnasnet0_5_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=True, num_upsample=3, num_flat=0):
+def mnasnet0_5_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=False, num_upsample=3, num_flat=0): # True
     backbone = torchvision.models.mnasnet0_5(pretrained=pretrained)
     return _mnasnet_pose_att(cmap_channels, paf_channels, upsample_channels, backbone, 1280, num_upsample, num_flat)
 
 
-def mnasnet0_75_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=True, num_upsample=3, num_flat=0):
+def mnasnet0_75_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=False, num_upsample=3, num_flat=0):
     backbone = torchvision.models.mnasnet0_75(pretrained=pretrained)
     return _mnasnet_pose_att(cmap_channels, paf_channels, upsample_channels, backbone, 1280, num_upsample, num_flat)
 
 
-def mnasnet1_0_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=True, num_upsample=3, num_flat=0):
+def mnasnet1_0_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=False, num_upsample=3, num_flat=0):
     backbone = torchvision.models.mnasnet1_0(pretrained=pretrained)
     return _mnasnet_pose_att(cmap_channels, paf_channels, upsample_channels, backbone, 1280, num_upsample, num_flat)
 
 
-def mnasnet1_3_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=True, num_upsample=3, num_flat=0):
+def mnasnet1_3_baseline_att(cmap_channels, paf_channels, upsample_channels=256, pretrained=False, num_upsample=3, num_flat=0):
     backbone = torchvision.models.mnasnet1_3(pretrained=pretrained)
     return _mnasnet_pose_att(cmap_channels, paf_channels, upsample_channels, backbone, 1280, num_upsample, num_flat)
